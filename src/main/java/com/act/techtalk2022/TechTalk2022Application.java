b1a1a4fb1b72a569668f02926254147085d163ef
ascenddevone <ascenddevone@gmail.com>
2022-09-12 22:45:30 +0700

    Updates configs

--- src/main/java/com/act/techtalk2022/TechTalk2022Application.java
+++ src/main/java/com/act/techtalk2022/TechTalk2022Application.java
@@ -2,10 +2,8 @@ package com.act.techtalk2022;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class TechTalk2022Application {
     
 	public static void main(String[] args) {
