352ad48f7ff2248f5077f356e0cfc594ac10088f
Trung-mta <trung.cntt9.mta@gmail.com>
2022-09-13 22:37:48 +0700

    add CORS

--- src/main/java/com/act/techtalk2022/config/WebMvcConfiguration.java
+++ src/main/java/com/act/techtalk2022/config/WebMvcConfiguration.java
@@ -3,6 +3,7 @@ package com.act.techtalk2022.config;
 import com.act.techtalk2022.interceptor.PayloadInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private final PayloadInterceptor payloadInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost:3000", "http://149.28.141.129");
+    }
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
