70352a64ecca6de033bc0d6be42e856047d1a90e
ascenddevone <ascenddevone@gmail.com>
2022-09-17 11:33:55 +0700

    Updates code

--- src/main/java/com/act/techtalk2022/util/CommonUtil.java
+++ src/main/java/com/act/techtalk2022/util/CommonUtil.java
@@ -3,8 +3,17 @@ package com.act.techtalk2022.util;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Slf4j
 public class CommonUtil {
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$");
 
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
 
 }
