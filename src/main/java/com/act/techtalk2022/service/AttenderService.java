a90b04b50546574a4f6fbaac71e33408f5179366
ascenddevone <ascenddevone@gmail.com>
2022-09-19 10:54:15 +0700

    Updates code

--- src/main/java/com/act/techtalk2022/service/AttenderService.java
+++ src/main/java/com/act/techtalk2022/service/AttenderService.java
@@ -1,14 +1,17 @@
 package com.act.techtalk2022.service;
 
+import com.act.techtalk2022.constant.ExceptionConstant;
 import com.act.techtalk2022.controller.request.CreateAttenderRequest;
 import com.act.techtalk2022.controller.request.UpdateAttenderRequest;
+import com.act.techtalk2022.exception.CommonException;
 import com.act.techtalk2022.repository.AttenderRepository;
 import com.act.techtalk2022.repository.enitiy.AttenderEntity;
+import com.act.techtalk2022.util.CommonUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,8 +39,7 @@ public class AttenderService {
     public List<AttenderEntity> getAllAttenders() {
 
         //Your code is here
-        List<AttenderEntity> list = attenderRepository.findAll();
-        return list;
+        return attenderRepository.findAll();
 
     }
 
@@ -61,7 +63,62 @@ public class AttenderService {
         attenderRepository.deleteById(attenderId);
     }
 
-    public Optional<AttenderEntity> findById(Integer attenderId) {
-        return attenderRepository.findById(attenderId);
+    public void validateUpdateAttender(UpdateAttenderRequest request) {
+        if (null == request.getFullName()
+                || request.getFullName().isEmpty()) {
+            log.error(ExceptionConstant.FULL_NAME_CANNOT_BE_BLANK_MESSAGE);
+
+            throw new CommonException(HttpStatus.BAD_REQUEST, ExceptionConstant.BAD_REQUEST_CODE, ExceptionConstant.FULL_NAME_CANNOT_BE_BLANK_MESSAGE);
+        }
+
+        if (null == request.getEmail()
+                || request.getEmail().isEmpty()
+                || !CommonUtil.validate(request.getEmail())) {
+            log.error(ExceptionConstant.EMAIL_CANNOT_BE_BLANK_OR_INVALID_MESSAGE);
+
+            throw new CommonException(HttpStatus.BAD_REQUEST, ExceptionConstant.BAD_REQUEST_CODE, ExceptionConstant.EMAIL_CANNOT_BE_BLANK_OR_INVALID_MESSAGE);
+        }
+
+        if (null == request.getDateOfBirth()) {
+
+            log.error(ExceptionConstant.DATE_OF_BIRTH_CANNOT_BE_BLANK_MESSAGE);
+
+            throw new CommonException(HttpStatus.BAD_REQUEST, ExceptionConstant.BAD_REQUEST_CODE, ExceptionConstant.DATE_OF_BIRTH_CANNOT_BE_BLANK_MESSAGE);
+
+        }
+    }
+
+    public void validateCreateAttender(CreateAttenderRequest request) {
+        if (null == request.getFullName()
+                || request.getFullName().isEmpty()) {
+            log.error(ExceptionConstant.FULL_NAME_CANNOT_BE_BLANK_MESSAGE);
+
+            throw new CommonException(HttpStatus.BAD_REQUEST, ExceptionConstant.BAD_REQUEST_CODE, ExceptionConstant.FULL_NAME_CANNOT_BE_BLANK_MESSAGE);
+        }
+
+        if (null == request.getEmail()
+                || request.getEmail().isEmpty()
+                || !CommonUtil.validate(request.getEmail())) {
+            log.error(ExceptionConstant.EMAIL_CANNOT_BE_BLANK_OR_INVALID_MESSAGE);
+
+            throw new CommonException(HttpStatus.BAD_REQUEST, ExceptionConstant.BAD_REQUEST_CODE, ExceptionConstant.EMAIL_CANNOT_BE_BLANK_OR_INVALID_MESSAGE);
+        }
+
+        if (null == request.getDateOfBirth()) {
+
+            log.error(ExceptionConstant.DATE_OF_BIRTH_CANNOT_BE_BLANK_MESSAGE);
+
+            throw new CommonException(HttpStatus.BAD_REQUEST, ExceptionConstant.BAD_REQUEST_CODE, ExceptionConstant.DATE_OF_BIRTH_CANNOT_BE_BLANK_MESSAGE);
+        }
+        log.info("== End to validate user==");
+    }
+
+    public void validateAttender(Integer attenderId) {
+        Optional<AttenderEntity> entity = attenderRepository.findById(attenderId);
+
+        if (!entity.isPresent()) {
+            log.error(ExceptionConstant.ATTENDER_IS_NOT_FOUND_MESSAGE);
+            throw new CommonException(HttpStatus.NOT_FOUND, ExceptionConstant.NOT_FOUND_CODE, ExceptionConstant.ATTENDER_IS_NOT_FOUND_MESSAGE);
+        }
     }
 }
